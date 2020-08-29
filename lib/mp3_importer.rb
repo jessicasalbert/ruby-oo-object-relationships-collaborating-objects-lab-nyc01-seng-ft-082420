aaf203a4d5158c28972a8d391c56e25a979c9e8f
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-29 18:11:16 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files(file)
+    file = self.path.split(",")
+    
+  end
+  
+
+  def import
+  end
+end
